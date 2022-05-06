benmoss/carvel-kapp-controller

Remove kbld, use docker build

hack/deploy-test.sh
@@ -2,7 +2,13 @@
 
 set -e
 
-./hack/build.sh && ytt -f config/ -f config-test/ | kbld -f- | kapp deploy -a kc -f- -c -y
+image="${KC_IMG:-carvel/kapp-controller:latest}"
+docker build -t "${image}" .
+if [ -n "${KC_IMG_PUSH}" ]; then
+  docker push -t "${image}"
+fi
+
+./hack/build.sh && ytt -f config/ -f config-test/ -v image="${image}" | kapp deploy -a kc -f- -c -y
 
 source ./hack/secretgen-controller.sh
 deploy_secretgen-controller

hack/deploy.sh
@@ -2,7 +2,13 @@
 
 set -e
 
-./hack/build.sh && ytt -f config/ | kbld -f- | kapp deploy -a kc -f- -c -y
+image="${KC_IMG:-carvel/kapp-controller:latest}"
+docker build -t "${image}" .
+if [ -n "${KC_IMG_PUSH}" ]; then
+  docker push -t "${image}"
+fi
+
+ytt -f config/ -v image="${image}" | kapp deploy -a kc -f- -c -y
 
 source ./hack/secretgen-controller.sh
 deploy_secretgen-controller

hack/dev-deploy.sh
@@ -21,5 +21,10 @@ tail -n +$run_image_start Dockerfile | \
    sed 's/usr\/local\/bin\///' \
    >> Dockerfile.dev
 
-ytt -f config/ -f config-dev-deploy/ | kbld -f- | kapp deploy -a kc -f- -c -y
+image="${KC_IMG:-carvel/kapp-controller:latest}"
+docker build -t "${image}" Dockerfile.dev
+if [ -n "${KC_IMG_PUSH}" ]; then
+  docker push -t "${image}"
+fi
 
+ytt -f config/ -v "${image}" | kapp deploy -a kc -f- -c -y

carvel-dev/kapp-controller

hack/deploy-test.sh
@@ -2,7 +2,13 @@
 
 set -e
 
-./hack/build.sh && ytt -f config/ -f config-test/ | kbld -f- | kapp deploy -a kc -f- -c -y
+image="${KC_IMG:-carvel/kapp-controller:dev}"
+docker build -t "${image}" .
+if [ -n "${KC_IMG_PUSH}" ]; then
+  docker push -t "${image}"
+fi
+
+./hack/build.sh && ytt -f config/ -f config-test/ -v image="${image}" | kapp deploy -a kc -f- -c -y
 
 source ./hack/secretgen-controller.sh
 deploy_secretgen-controller

hack/deploy.sh
@@ -2,7 +2,13 @@
 
 set -e
 
-./hack/build.sh && ytt -f config/ | kbld -f- | kapp deploy -a kc -f- -c -y
+image="${KC_IMG:-carvel/kapp-controller:dev}"
+docker build -t "${image}" .
+if [ -n "${KC_IMG_PUSH}" ]; then
+  docker push -t "${image}"
+fi
+
+ytt -f config/ -v image="${image}" | kapp deploy -a kc -f- -c -y
 
 source ./hack/secretgen-controller.sh
 deploy_secretgen-controller

hack/dev-deploy.sh
@@ -21,5 +21,10 @@ tail -n +$run_image_start Dockerfile | \
    sed 's/usr\/local\/bin\///' \
    >> Dockerfile.dev
 
-ytt -f config/ -f config-dev-deploy/ | kbld -f- | kapp deploy -a kc -f- -c -y
+image="${KC_IMG:-carvel/kapp-controller:dev}"
+docker build -t "${image}" Dockerfile.dev
+if [ -n "${KC_IMG_PUSH}" ]; then
+  docker push -t "${image}"
+fi
 
+ytt -f config/ -v "${image}" | kapp deploy -a kc -f- -c -y